d8d2a5d33f55f33402c7470a692c13b64002c69f
lockecole111 <lockecole111@gmail.com>
2020-04-14 14:55:35 +0800

    update

--- core/api.py
+++ core/api.py
@@ -1,3 +1,29 @@
+import json
+from client import Client
+from ipfs import IPFS
+
 class Api:
+
     def __init__(self):
-        pass
+        self.ipfs = IPFS()
+        self.client = Client()
+    def add(self, file_path):
+        add_info = self.ipfs.add(file_path)
+        hash = add_info['Hash']
+        name = add_info['Name']
+        size = add_info['Size']
+        self.client.add(name = name, hash = hash, size = size)
+
+    def download(self, file_hash):
+        self.ipfs.get(file_hash)
+        
+    def delete(self, file_hash):
+        self.client.delete(hash = file_hash)
+
+    def list_files(self):
+        return self.client.list_files()
+
+if __name__ == '__main__':
+    api = Api()
+    print(api.list_files())
+
--- core/ipfs.py
+++ core/ipfs.py
@@ -1,4 +1,5 @@
 import ipfsapi
+import json
 
 class IPFS:
     def __init__(self):
@@ -7,7 +8,7 @@ class IPFS:
                 self.config = json.loads(f.read())
         except FileNotFoundError:
             raise
-
+        self.DOWNLOAD_PATH = self.config['CLIENT']['DOWNLOAD_PATH']
         self.HOST = self.config['IPFS']['HOST']
         self.PORT = self.config['IPFS']['PORT']
         try:
@@ -18,3 +19,5 @@ class IPFS:
     def add(self, file_path):
 
         return self.api.add(file_path)
+    def get(self, file_hash, file_path):
+        return self.api.get(file_hash, filepath = self.DOWNLOAD_PATH)
